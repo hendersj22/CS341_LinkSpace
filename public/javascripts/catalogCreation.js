930b95500bad1cf7717f39a203f8a53d49871946
lute47lillo <lutelillocs47@gmail.com>
2021-04-01 07:13:50 -0700

    Half link validation, creation catalog addition and jade fix

--- public/javascripts/catalogCreation.js
+++ public/javascripts/catalogCreation.js
@@ -1,30 +1,35 @@
 $(document).ready(function() {
 
-    $(".catalog_add").click(function() {
-
-    });
+    // Create more link boxes to add more than one link
+    // MAKE IT MORE PRETTY
+    $("#addLinks").click(function() {
+        var input = document.createElement("input");
+        input.setAttribute("type", "text"); 
+        input.setAttribute("class", "catalog_link");
+        document.getElementById('catalog').appendChild(input);
+    })
 
     $("#save").click(function() {
         const title = $(".catalog_title").val();
         var urls = $(".catalog_link");
         var descriptions = $(".catalog_description");
-
+        
         var reqBody = {
             "Name": title,
             "Links": []
         }
-
+        
         for(let i = 0; i < urls.length; i++){
-            let url = urls[i].val();
-            let description = descriptions[i].val();
+            let url = urls[i].value;
+            //let description = descriptions[i].value;
+            console.log(url);  
 
             let link = {
                 "URL": url,
-                "Description": description
+                //"Description": description
             }
             reqBody["Links"].push(link);
         }
-
 
         $.post("/catalog/create", reqBody, function(catalogID) {
             window.location = "/catalog/" + catalogID;
@@ -32,5 +37,6 @@ $(document).ready(function() {
         .fail(function() {
             alert("Couldn't create catalog")
         })
+
     })
 })
---
+++ public/javascripts/viewCatalog.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    // We use this class to hold all the catalog previews on home page and trending and mycatalogs
+    // Need if statement so you can only click on grid-items while one these certain pages
+    let catalogEntry = $(".grid-item");
+    if(window.location == "/" || window.location == "/trending" || window.location == "/catalogs") {
+        catalogEntry.click(function(event) {
+            //this event target will differentiate the catalogs from each other, and show which specific element was clicked
+
+            //this is assuming the (event.target.id == the catalog id) through another script
+            $.get("/catalogs/" + event.target.id + "/info", function(catalogInfo) {
+                window.location = "/catalogs/" + catalogInfo["Catalog_ID"];
+                $(".catalog_title").text(catalogInfo["Name"]);
+                $(".catalog_author").text(catalogInfo["Author"]); // need to add this to the response body
+
+                //iterate through the array of links, where we only care about the URL and description
+                var linksArrayLength = catalogInfo.Links.length;
+                for(let i = 0; i < linksArrayLength; i++)
+                {
+                    let url = catalogInfo["Links"][i].URL;
+                    let description = catalogInfo["Links"][i].Description;
+
+                    var link = $('<a />');
+                    link.attr('href', url);
+                    link.text(url); // there's two options for our entries. Do we want the hyperlinked text to be of the whole URL, or the description?
+
+                    var entryDescription = $('<p />');
+                    entryDescription.text(description);
+
+                    $(".catalog").append(link);
+                    $(".catalog").append(entryDescription);
+                }
+            })
+            .fail(function() {
+                alert("Server error");
+            });
+        });
+    }
+    // Assuming this page's url is linkspace.com/catalogs/{idOfChosen}
+});
