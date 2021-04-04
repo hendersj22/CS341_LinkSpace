4b747a0d295a03deef096fc1de3a25f4eeb65cb0
lamlinda <linda.nguyen0819@gmail.com>
2021-04-04 14:34:22 -0700

    Changes to searchBar.js to display catalog information

--- public/javascripts/searchBar.js
+++ public/javascripts/searchBar.js
@@ -37,4 +37,28 @@ $(document).ready(function () {
         window.location = "/logout";
     })
 
+       //make changes to the HTML to display the search results
+function searchEntered(){
+    $.post("/search/list", {"Order": "ascending"}, function(data, status){
+
+        for(var i=0; i<data.length; i++){
+            var Catalog_ID = data[i]["Catalog_ID"];
+            var Catalog_Title = data[i]["Name"];
+            var Num_Links = data[i]["Links"].length;
+            var Author = data[i]["Author"];
+  
+          $('#catContainer').append(
+              `<div class="grid-item" id=${Catalog_ID}>
+                 <div class="catalog">
+                   <p class="set-title">${Catalog_Title}</p>
+                   <p class="set-link">${Num_Links} links</p>
+                   <p class="set-author">${Author}</p>
+                 </div>
+                 <div class="image"></div><a class="fill-div" href="#"></a>
+               </div>`);
+        }
+
+        });
+}
+
 });
